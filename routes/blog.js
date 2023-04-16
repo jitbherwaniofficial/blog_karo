b58dd2babc85e5dbbffb6fcb3eae799caf68b6e5
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-04-16 18:37:40 +0530

    Updated

--- routes/blog.js
+++ routes/blog.js
@@ -7,41 +7,17 @@ const Comment = require('../model/comment');
 const router = Router();
 
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, path.resolve(`./public/uploads`))
-//     },
-//     filename: function (req, file, cb) {
-//       const fileName = `${Date.now()}-${file.originalname}`;
-//       cb(null, fileName)
-//     }
-//   })
-  
-// const upload = multer({ storage: storage })
-
-const FILE_TYPE_MAP = {
-    'image/png':'png',
-    'image/jpeg':'jpeg',
-    'image/jpg':'jpg',
-}
-
-
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        const isValid = FILE_TYPE_MAP[file.mimetype];
-        let uploadError = new Error('Not A Valid Image Type');
-        if(isValid){
-            uploadError = null;
-        }
-        cb(uploadError, 'public/uploads')
+      cb(null, path.resolve(`./temp/uploads`))
     },
     filename: function (req, file, cb) {
-        const fileName = file.originalname.split(' ').join('-');
-        const extension = FILE_TYPE_MAP[file.mimetype];
-        cb(null, `${fileName}-${Date.now()}.${extension}`)
+      const fileName = `${Date.now()}-${file.originalname}`;
+      cb(null, fileName)
     }
-})
-const uploadOption = multer({ storage: storage })
+  })
+  
+const upload = multer({ storage: storage })
 
 
 router.get('/add-new', (req,res) => {
@@ -60,21 +36,13 @@ router.get('/:id', async(req,res) => {
     })
 })
 
-router.post('/', uploadOption.single("coverImage"),async(req,res) => {
-    const file = req.file;
-    if(!file){
-        return res.status(400).send("No Image in the Request");
-    }
-
-    const fileName = req.file.filename;
-    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+router.post('/', upload.single("coverImage"),async(req,res) => {
     const {title, body } = req.body;
     const blog = await Blog.create({
         body,
         title,
         createdBy: req.user._id,
-        coverImageURL: `${basePath}${fileName}`
-        // coverImageURL: `/uploads/${req.file.filename}`
+        coverImageURL: `/uploads/${req.file.filename}`
     })
     return res.redirect(`/blog/${blog._id}`)
 })
